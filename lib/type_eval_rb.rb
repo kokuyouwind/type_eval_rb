968c4cfd5a4c09fe323fba5176192b1e5989bd90
kokuyouwind <kokuyouwind@gmail.com>
2024-06-02 22:24:37 +0900

    initial commit

---
+++ lib/type_eval_rb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "type_eval_rb/version"
+
+module TypeEvalRb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/type_eval_rb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TypeEvalRb do
+  it "has a version number" do
+    expect(TypeEvalRb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
